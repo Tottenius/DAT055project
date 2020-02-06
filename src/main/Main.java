5cf7dc0241b4cddb06f90986a36aa956b6979488
anton <anton@10.0.17.141>
2020-02-06 21:56:57 +0100

    added introscreen to own method

--- src/main/Main.java
+++ src/main/Main.java
@@ -14,19 +14,9 @@ public class Main {
 
 	public static void main(String[] args) throws MalformedURLException {
 		
-		JWindow window = new JWindow();
-		window.getContentPane().add(
-		    new JLabel("", new ImageIcon(new URL("https://i.imgur.com/fVefx9v.gif")), SwingConstants.CENTER)); //intro screen maybe use splashscreen class instead
-		window.setBounds(400, 100, 1200, 800);
-		window.setVisible(true);
-		try {
-		    Thread.sleep(5000);
-		} catch (InterruptedException e) {
-		    e.printStackTrace();
-		}
-		window.dispose();
+		new IntroScreen();
 		
-		new GameWindowTemp();
+		//new GameWindowTemp();
 	}
 
 }
