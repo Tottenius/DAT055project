2e7e82e400b2ff418ae009baf83d95a0f49bee13
anton <anton@LAPTOP-V1OAL44R.lan>
2020-02-10 14:34:43 +0100

    updated assets

---
+++ src/assetclasses/Spikes.java
@@ -0,0 +1,16 @@
+package assetclasses;
+
+
+//Spike to dodge or player will die
+public class Spikes extends Asset {
+
+	private static final String path = "src/assets/tile.png";
+
+	
+	public Spikes(int position) {
+		super(position, path);
+	}
+	
+	
+
+}
