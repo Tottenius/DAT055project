7af05fecbca5edec1eff4622e7c919d2d5e46232
anton <anton@Kungens-Dator.lan>
2020-02-27 16:07:40 +0100

    Testing javadoc comments

--- src/assetclasses/Asset.java
+++ src/assetclasses/Asset.java
@@ -7,14 +7,23 @@ import Controller.Direction;
 import viewer.GamePanel;
 
 public interface Asset {
-    public Image getImage();  //get an image at an specified location in our asset array intractable
     
-    //Asset Attributes
+	public Image getImage();  //get an image at an specified location in our asset array intractable
+    
+    // ------------------------------------------------ Asset Attributes ------------------------------------------------ \\
+    
+    //Can be killed, in this case it only refers to the player, as a player currently can't kill anything
     public boolean killable();
+    //Can kill something that is killable
     public boolean canKill();
+    //Is interactble, something the player can interact with for exemple a treasure
     public boolean intractable();
+    //Something any moving entity that moves can walk over
     public boolean canWalkOn();
-    public boolean hasDirections(Direction d);  
+    //Entity that moves in multible directions
+    public boolean hasDirections(Direction d);
+    
+    //Animations
     public void setPrevPos();
     public void paintAsset(Graphics g, GamePanel gp);
 }
--- src/viewer/StopWatch.java
+++ src/viewer/StopWatch.java
@@ -1,16 +1,33 @@
 package viewer;
 
+/**
+ * A class that can track time for different task.
+ *  
+ * @author Anton(ska man skriva alla i sin gruppe eller den som gjord det?)
+ * @param
+ * @see 
+ * @value
+ * @version 1.0
+ * */
+
 public class StopWatch {
 
 	static long Start_time;
 	static long End_time;
 
- 
+	/**
+	   * This method starts the timer
+	   * @return Nothing.
+	   */
 	public static void start() {
 	
 		Start_time = System.currentTimeMillis();
 	}
 
+	/**
+	   * This method stops the timer
+	   * @return Time in seconds
+	   */
 	public static long stopTimer() {
 		
 	  End_time = System.currentTimeMillis();
