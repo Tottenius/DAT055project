f6477d920ff13e3299a220bb52fd7e595dd34548
Tor Clementz <clementz@student.chalmers.se>
2020-03-02 17:05:19 +0100

    Man kan nu läsa in och skriva till en leaderboard-fil.

--- src/server/UdpServer.java
+++ src/server/UdpServer.java
@@ -2,13 +2,46 @@ package server;
 
 import java.io.*;
 import java.net.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
  
 
-public class UdpServer implements Runnable{ 
+public class UdpServer implements Runnable{
+	
+	private String leaderboard;
+	private StringBuilder stringb;
+	private final String leaderboardPath = "src/leaderboard/leaderboard.txt";
+	
+	//private File leaderboardFile = new File("src/leaderboard/leaderboard.txt");
+	
+	public UdpServer() {
+		readInLeaderboard();
+	}
+	
+	private void readInLeaderboard() {
+		try {
+			leaderboard = new String(Files.readAllBytes(Paths.get(leaderboardPath)));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	private void writeToLeaderboard(StringBuilder stringBuilder) {
+		FileWriter write = null;
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(leaderboardPath, true));
+			writer.append(stringBuilder);
+			writer.newLine();
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
     public void startServer() throws IOException { 
-    	System.out.println(" Vi försöker starta servern");
+    	//System.out.println(" Vi försöker starta servern");
         // Step 1 : Create a socket to listen at port 4567 
         DatagramSocket socket = new DatagramSocket(4567); 
         byte[] receive = new byte[65535]; 
@@ -23,7 +56,8 @@ public class UdpServer implements Runnable{
             // Step 3 : revieve the data in byte buffer. 
             socket.receive(DpReceive); 
   
-            System.out.println("Client:-" + data(receive)); 
+           // System.out.println("Client:-" + data(receive)); 
+            writeToLeaderboard(data(receive));
   
             // Exit the server if the client sends "bye" 
             if (data(receive).toString().equals("bye")) 
