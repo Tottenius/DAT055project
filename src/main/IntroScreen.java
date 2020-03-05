643bc0875ae66cce6170f568e80d1a49be692f02
anton <antonskebaber@gmail.com>
2020-03-05 03:18:34 +0100

    added intro totally not copyright

--- src/main/IntroScreen.java
+++ src/main/IntroScreen.java
@@ -1,21 +1,30 @@
 package main;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JWindow;
+import javax.swing.SwingConstants;
+
 import viewer.GameWindowTemp;
 
 public class IntroScreen {
 
 	@SuppressWarnings("unused")
 	public static void main(final String[] args) throws MalformedURLException {
-
-		/*
-		 * JWindow window = new JWindow(); window.getContentPane().add( new JLabel("",
-		 * new ImageIcon(new URL("https://i.imgur.com/fVefx9v.gif")),
-		 * SwingConstants.CENTER)); //intro screen maybe use splashscreen class instead
-		 * window.setBounds(400, 100, 1200, 800); window.setVisible(true); try {
-		 * Thread.sleep(5000); } catch (InterruptedException e) { e.printStackTrace(); }
-		 * window.dispose();
-		 */
+	
+		  JWindow window = new JWindow(); window.getContentPane().add( new JLabel("",
+		  new ImageIcon(new URL("https://i.imgur.com/KuN4n3N.gif")),SwingConstants.CENTER)); 
+		  window.setBounds(400, 100, 1200, 800); window.setVisible(true); try {
+		  Thread.sleep(2500); } catch (InterruptedException e) { e.printStackTrace(); }
+		 window.dispose();
+		 
 
 		new GameWindowTemp("level1");
 
