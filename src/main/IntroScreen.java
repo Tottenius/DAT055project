515236bba963e6537d962571c6c8656e0f011ffe
anton <antonskebaber@gmail.com>
2020-03-05 03:18:55 +0100

    cleaning up

--- src/main/IntroScreen.java
+++ src/main/IntroScreen.java
@@ -1,12 +1,8 @@
 package main;
 
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+
 import java.net.MalformedURLException;
 import java.net.URL;
-
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JWindow;
